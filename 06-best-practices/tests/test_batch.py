f926281a91ed3864c2b69e65d9d903fb932f339f
hamid <hamidjahani999@gmail.com>
2023-07-11 19:09:18 +0330

    add best practice

---
+++ 06-best-practices/tests/test_batch.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from datetime import datetime
+
+def dt(hour, minute, second=0):
+    return datetime(2022, 1, 1, hour, minute, second)
+
+data = [
+    (None, None, dt(1, 2), dt(1, 10)),
+    (1, None, dt(1, 2), dt(1, 10)),
+    (1, 2, dt(2, 2), dt(2, 3)),
+    (None, 1, dt(1, 2, 0), dt(1, 2, 50)),
+    (2, 3, dt(1, 2, 0), dt(1, 2, 59)),
+    (3, 4, dt(1, 2, 0), dt(2, 2, 1)),
+]
+
+columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
+df = pd.DataFrame(data, columns=columns)
+
+# Define the expected output
+expected_data = [
+    (None, None, dt(1, 2), dt(1, 10)),
+    (1, None, dt(1, 2), dt(1, 10)),
+    (1, 2, dt(2, 2), dt(2, 3)),
+    (None, 1, dt(1, 2, 0), dt(1, 2, 50)),
+    (2, 3, dt(1, 2, 0), dt(1, 2, 59)),
+    (3, 4, dt(1, 2, 0), dt(2, 2, 1)),
+]
+
+expected_df = pd.DataFrame(expected_data, columns=columns)
+
+# Compare the expected output with the actual dataframe
+assert df.equals(expected_df)
